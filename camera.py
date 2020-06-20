d100545a46b6cf3ebe38090182dfac5e3a2dddf1
satheesh22g <satheeshgajula22.com>
2020-06-20 16:44:15 +0530

    add webcam to flask

---
+++ camera.py
@@ -0,0 +1,63 @@
+import cv2
+import numpy as np
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainer/trainer.yml')
+cascadePath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+font = cv2.FONT_HERSHEY_SIMPLEX
+#iniciate id counter
+id = 0
+# names related to ids: example ==> Marcelo: id=1,  etc
+names = ['None', 'Satheesh', 'Sashi'] 
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()
+
+    # returns camera frames along with bounding boxes and predictions
+    def get_frame(self):
+        ret, img = self.video.read()
+        img = cv2.flip(img, 1)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale( 
+            gray,
+            scaleFactor = 1.2,
+            minNeighbors = 5,
+        )
+        for(x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+            id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+            
+            # If confidence is less them 100 ==> "0" : perfect match 
+            if (confidence < 100):
+                id = names[id]
+                confidence = "  {0}%".format(round(100 - confidence))               
+            else:
+                id = "unknown"
+                confidence = "  {0}%".format(round(100 - confidence))
+            
+            cv2.putText(
+                        img, 
+                        str(id), 
+                        (x+5,y-5), 
+                        font, 
+                        1, 
+                        (255,255,255), 
+                        2
+                    )
+            cv2.putText(
+                        img, 
+                        str(confidence), 
+                        (x+5,y+h-5), 
+                        font, 
+                        1, 
+                        (255,255,0), 
+                        1
+                    )  
+        
+
+        _, jpeg = cv2.imencode('.jpg', img)
+        return jpeg.tobytes()
